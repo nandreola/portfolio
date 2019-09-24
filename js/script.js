9f6c23a8b1718d8564a608eab0e23bfa25651975
Natalia <nathy.andreola@gmail.com>
2019-09-24 02:04:28 -0400

    added hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 // setup smooth scroll 
 const scroll = new SmoothScroll( 'a[href *="#"]' , {speed: 600} );
+
+// Toggle hamburger menu to open/close 
+$(function(){
+    $(".hamburger").click(function () {
+        $(".navMobile").slideToggle("slow");
+        $(".hamburger i").toggleClass("fa-bars fa-times");
+    });
+});
