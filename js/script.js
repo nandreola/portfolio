1c6e68cf0651f2894cc52a4b0fd46956ba92fda4
Natalia <nathy.andreola@gmail.com>
2019-09-24 22:52:06 -0400

    added animation to work section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-// setup smooth scroll 
-const scroll = new SmoothScroll( 'a[href *="#"]' , {speed: 600} );
-
 // Toggle hamburger menu to open/close 
 $(function(){
-    $(".hamburger").click(function () {
+    $(".hamburger").on('click', function () {
         $(".navMobile").slideToggle("slow");
         $(".hamburger i").toggleClass("fa-bars fa-times");
     });
+
+    // Initializer for zoom-in animation  
+    AOS.init();
+
+    // setup smooth scroll 
+    const scroll = new SmoothScroll('a[href *="#"]', { speed: 600 });
 });
